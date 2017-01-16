4eff5fa7a85aa642a72a02d6b73c174440f7e8f1
Rameet Sekhon <rameetsekhon@yahoo.ca>
2017-01-16 11:37:14 -0500

    changed hellorandy file

--- lab01/hellorandy.java
+++ lab01/hellorandy.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         
-        System.out.println("Hello, Randy. I know you are watching.");
+        System.out.println("Hello, Randy. I know you are watching. Big brother is always watching.");
     }
 
 }
