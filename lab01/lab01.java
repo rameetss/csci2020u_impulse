824c4383f09a0b91c17b00c71a0b8c24464b83fe
Eyaz Rehman <scrappycocos@hotmail.com>
2017-01-16 11:26:32 -0500

    Modified lab01.java

--- lab01/lab01.java
+++ lab01/lab01.java
@@ -1,5 +1,5 @@
 public class Lab01 {
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		System.out.println("Hello world! We gonna have some conflicts in this one!");
 	}
 }
